835f868eab53e494c2fd93c816c335f07db62466
diegoingaramo <diegoingaramo@gmail.com>
2016-03-09 23:44:49 -0300

    angular style loading added

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,31 @@
 
 function AppController($scope) {
     
+  $scope.locationText = "";
+    
+  $scope.searchLocations = function() {
+
+  };
+    
 };
 
 /* End main controller definition */
 
 var app = angular.module('mainModule', []).controller('AppController', ['$scope', AppController]);
+
+
+app.config(function($httpProvider){
+  $httpProvider.interceptors.push(function($q) {
+    return {
+     'request': function(config) {
+         $('#dvLoading').show();
+         return config;
+      },
+
+      'response': function(response) {
+         $('#dvLoading').hide();
+         return response;
+      }
+    };
+  });
+});
